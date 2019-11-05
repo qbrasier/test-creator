de34db01b4bd2e8f7f1f3e6f6054a12795314b5a
Quentin Brasier <qbrasier@gmail.com>
2019-11-05 06:17:23 -0600

    added TestController added test/addTest.html and test/index.html got CRUD working for Test

---
+++ src/main/java/quentin/testcreator/controllers/TestController.java
@@ -0,0 +1,87 @@
+package quentin.testcreator.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import quentin.testcreator.models.Test;
+import quentin.testcreator.models.data.TestDao;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("t-edit")
+public class TestController {
+
+    @Autowired
+    TestDao testDao;
+
+    @RequestMapping("")
+    public String index(Model model){
+        model.addAttribute("tests", testDao.findAll());
+        model.addAttribute("title", "Tests");
+        return "test/index";
+    }
+
+    @RequestMapping(value = "addTest", method = RequestMethod.GET)
+    public String addTest(Model model){
+        model.addAttribute("test", new Test());
+        model.addAttribute("title", "Create New Test");
+        return "test/addTest";
+    }
+
+    @RequestMapping(value = "addTest", method = RequestMethod.POST)
+    public String addTest(Model model, @ModelAttribute @Valid Test test, Errors errors){
+        if(errors.hasErrors()){
+            model.addAttribute("test", test);
+            model.addAttribute("title", "Create New Test");
+            return "test/addTest";
+        }
+        testDao.save(test);
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "delete/{Id}", method = RequestMethod.GET)
+    //@DeleteMapping(value = "delete/{Id}")
+    public String removeTest(Model model, @PathVariable int Id) {
+
+        System.out.println("trying to delete " + Id );
+        testDao.deleteById(Id);
+
+        return "redirect:/t-edit/";
+    }
+
+    @RequestMapping(value = "edit/{Id}", method = RequestMethod.GET)
+    public String editTestName(Model model, @PathVariable int Id) {
+
+        System.out.println("trying to edit " + Id );
+        Optional<Test> optionalTest = testDao.findById(Id);
+        if(!optionalTest.isPresent()){
+            return "redirect:/t-edit/";
+        }
+        Test test = optionalTest.get();
+        model.addAttribute("test", test);
+        model.addAttribute("title", "Edit Test Name");
+        return "test/addTest";
+    }
+
+    @RequestMapping(value = "edit/{Id}", method = RequestMethod.POST)
+    public String editQuestion(Model model, @ModelAttribute @Valid Test test, Errors errors) {
+        System.out.println("editing test");
+        if(errors.hasErrors()){
+            System.out.println("error in test");
+            model.addAttribute("test", test);
+            model.addAttribute("title", "Edit Test Name");
+            model.addAttribute("error", errors);
+            return "test/addTest";
+        }
+        testDao.save(test);
+        return "redirect:/t-edit/";
+    }
+
+}
